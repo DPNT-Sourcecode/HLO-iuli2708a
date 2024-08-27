24487bdafe9794aebc5aec131ddb0a4b0473c9a5
sbx_user1051 <sbx_user1051@169.254.65.101>
2024-08-27 20:03:52 +0000

    Tue Aug 27 20:03:52 UTC 2024

---
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using BeFaster.App.Solutions.HLO;
+using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestFixture]
+    public class HelloSolutionTest
+    {
+        [TestCase("x", ExpectedResult = "Hello, World!")]
+        public string ComputeHello(string x)
+        {
+            return HelloSolution.Hello(x);
+        }
+    }
+}
